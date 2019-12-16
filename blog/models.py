076d5e83e33dc9f6c0d78affdccb0282b070a0b0
Rajat Shenoy <rajat.shenoy@spit.ac.in>
2019-12-16 11:14:22 +0530

    Added post and comment functions

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    # def publish(self):
+    #     self.published_date = timezone.now()
+    #     self.save()
+
+    def __str__(self):
+        return self.question
+
+
+class Answers(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    
+    # def publish(self):
+    #     self.save()
+
+    def __str__(self):
+        return self.text
